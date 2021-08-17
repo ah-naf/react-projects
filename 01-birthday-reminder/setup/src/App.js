48fa829f4568c413eff6c031d07b2aac41a1a3ff
Ahnaf <sheikhahnafshifat@gmail.com>
2021-08-17 21:50:19 +0600

    Project-1 completed

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,48 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+
+// Function Component
+// function App() {
+//   const [people, setPeople] = useState(data);
+//   return (
+//     <main>
+//       <section className="container">
+//         <h3>{people.length} birthdays tody</h3>
+//         <List people={people} />
+//         <button onClick={() => setPeople([])}>Clear All</button>
+//       </section>
+//     </main>
+//   );
+// }
+
+// Class Component
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: data,
+    }
+    this.clearAll = this.clearAll.bind(this);
+  }
+  clearAll() {
+    this.setState({people: []});
+  }
+  render() {
+    return (
+      <main>
+        <section className="container">
+          <h3>
+          {
+            this.state.people.length === 0 ? '0 birthday today' : this.state.people.length + ' birthdays today'
+          }
+          </h3>
+          <List people={this.state.people} />
+          <button onClick={this.clearAll}>Clear All</button>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default App;

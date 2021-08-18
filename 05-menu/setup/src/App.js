b7938cf2061743a0b349e01e9a29b1ee0d377e7d
Ahnaf <sheikhahnafshifat@gmail.com>
2021-08-18 21:56:14 +0600

    Project-5 completed

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,72 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  return <h2>menu project setup</h2>;
+const allCategories =['all', ...new Set(items.map((item) => item.category))];
+
+// Functional Component
+
+// function App() {
+//   const [menuItems, setMenuItems] = useState(items);
+//   const [categories, setCategories] = useState(allCategories);
+
+//   const filterItems = (category) => {
+//     if(category === 'all') {
+//       setMenuItems(items);
+//       return;
+//     }
+//     const newItems = items.filter((item) => item.category === category);
+//     setMenuItems(newItems);
+//   }
+
+//   return <main>
+//     <section className="menu section">
+//       <div className="title">
+//         <h2>our menu</h2>
+//         <div className="underline"></div>
+//       </div>
+//       <Categories categories={categories} filterItems={filterItems} />
+//       <Menu items={menuItems} />
+//     </section>
+//   </main>;
+// }
+
+
+
+// Class Component
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuItems: items,
+      categories: allCategories,
+    };
+    this.filterItems = this.filterItems.bind(this);
+  }
+
+  filterItems(category) {
+    if(category === 'all') {
+      this.setState({menuItems: items});
+      return ;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    this.setState({menuItems: newItems});
+  }
+
+  render() {
+    return (
+      <main>
+        <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <Categories categories={this.state.categories} filterItems={this.filterItems} />
+          <Menu items={this.state.menuItems} />
+        </section>
+      </main>   
+    );
+  }
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+
+// Functional Component
+
+// const Categories = ({categories, filterItems }) => {
+//   return (
+//     <div className="btn-container">
+//       {
+//         categories.map((category, index) => {
+//           return <button className="filter-btn" key={index} type="button"
+//           onClick={() => filterItems(category)} >{category}</button>
+//         })
+//       }
+//     </div>
+//   );
+// };
+
+
+
+// Class Component
+
+class Categories extends React.Component {
+  render() {
+    return (
+      <div className="btn-container">
+        {
+          this.props.categories.map((category, index) => {
+            return <button className="filter-btn" key={index} type="button"
+            onClick={() => this.props.filterItems(category)} >{category}</button>
+          })
+        }
+      </div>      
+    );
+  }
+}
 
 export default Categories;
